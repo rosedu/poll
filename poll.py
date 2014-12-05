88ea3faa7ebe135ae1cd1eb324fe20bb95b126a2
Alex Morega <alex@grep.ro>
2014-12-05 17:55:42 +0200

    models

--- poll.py
+++ poll.py
@@ -6,6 +6,50 @@ app = flask.Flask(__name__)
 db = SQLAlchemy(app)
 
 
+class Person(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String, unique=True)
+    secretkey = db.Column(db.String)
+
+
+group_member = db.Table(
+    'group_member',
+    db.Column('group_id', db.ForeignKey('group.id')),
+    db.Column('person_id', db.ForeignKey('person.id')),
+)
+
+
+class Group(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    slug = db.Column(db.String, unique=True)
+
+    members = db.relationship('Person', secondary=group_member)
+
+
+class Poll(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    slug = db.Column(db.String, unique=True)
+    isopen = db.Column(db.Boolean, default=False, nullable=False)
+    isvisible = db.Column(db.Boolean, default=True, nullable=False)
+    votes_yee = db.Column(db.Integer, default=0, nullable=False)
+    votes_nay = db.Column(db.Integer, default=0, nullable=False)
+    votes_abs = db.Column(db.Integer, default=0, nullable=False)
+
+
+class PollMember(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    person_id = db.Column(db.ForeignKey('person.id'))
+    poll_id = db.Column(db.ForeignKey('poll.id'))
+    voted = db.Column(db.Boolean, default=False, nullable=False)
+
+
 @app.route('/')
 def home():
     return 'hi'
